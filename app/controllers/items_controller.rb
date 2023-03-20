890ffa26bdc24af973bb249fd0d374d9a13e9a69
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-03-20 12:23:06 +0300

    completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items, include: :user
+      end
+    
+      private
+    
+      def render_not_found_response
+        render json: { error: "item  not found" }, status: :not_found
+      end
 end
